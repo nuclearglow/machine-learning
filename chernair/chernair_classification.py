43914f2340fd21a4454abe1ba6b9dc0400d60499
Stefan Arnau <fischmechanik@gmail.com>
2020-11-04 00:12:16 +0100

    cherair classification modified

--- chernair/chernair_classification.py
+++ chernair/chernair_classification.py
@@ -9,6 +9,7 @@ import geopy.distance
 import datetime
 import multiprocessing
 import itertools
+import tensorflow as tf
 from tensorflow import keras
 from pytz import timezone
 from sklearn.linear_model import Ridge
@@ -19,6 +20,7 @@ from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import RandomizedSearchCV, GridSearchCV, train_test_split
 from pathlib import Path
 import joblib
+from sklearn.metrics import accuracy_score, confusion_matrix, precision_score
 
 # current directory with data file
 preprocessed_data_path = os.path.abspath(
@@ -28,22 +30,25 @@ preprocessed_data_path = os.path.abspath(
 # Read data
 data = joblib.load(preprocessed_data_path)
 
-
-# 1 Klassifikator-Modell
-#   cherntime, isotop-konzentration ->Schätzt, welche Stadt
+# X-data: cherntime, isotope-konzentrations 
 X = data[
     ["cherntime", "I-131_interpolated", "Cs-134_interpolated", "Cs-137_interpolated"]
 ].to_numpy()
+
+# Scaale X-data
 scaler = StandardScaler()
 X = scaler.fit_transform(X)
 
+# Get labels
 y = data["city_code"].to_numpy()
 
-X_train_full, X_test, y_train_full, y_test = train_test_split(X, y, test_size=0.2)
+# Split data in stratified manner
+X_train_full, X_test, y_train_full, y_test = train_test_split(X, y, test_size=0.2, stratify=y)
 X_train, X_train_valid, y_train, y_train_valid = train_test_split(
-    X_train_full, y_train_full, test_size=0.2
+    X_train_full, y_train_full, test_size=0.3, stratify=y_train_full
 )
 
+# Determine number of classes
 n_cities = np.unique(y).shape[0]
 
 # Function builds and returns a regression model
@@ -63,10 +68,8 @@ def build_model(n_hidden=1, n_neurons=30, learning_rate=3e-3, input_shape=[4]):
         optimizer=optimizer,
         metrics=["accuracy"],
     )
-
     model.summary()
     return model
-
 
 # use a wrapper to get a scikit learn RegressorModel for the Keras factory method
 keras_clf = keras.wrappers.scikit_learn.KerasClassifier(
@@ -81,8 +84,10 @@ parameter_distribution = {
     "input_shape": [X_train.shape[1]],
 }
 
+# Init grid-search
 grid_search_cv = GridSearchCV(keras_clf, parameter_distribution, cv=3)
 
+# Perform grid search
 grid_search_cv.fit(
     X_train,
     y_train,
@@ -91,16 +96,18 @@ grid_search_cv.fit(
     callbacks=[keras.callbacks.EarlyStopping(patience=10)],
 )
 
+# Get best params
 best_params = grid_search_cv.best_params_
 best_score = grid_search_cv.best_score_
 
+# Build a fresh optimized model
 optimized_model = build_model(
     n_hidden=best_params["n_hidden"],
     n_neurons=best_params["n_neurons"],
     learning_rate=best_params["learning_rate"],
 )
 
-# simple
+# Fit the optimized model
 optimized_model.fit(
     X_train,
     y_train,
@@ -109,4 +116,57 @@ optimized_model.fit(
     callbacks=[keras.callbacks.EarlyStopping(patience=10)],
 )
 
+# Save the optimized model
 optimized_model.save("trained_models/model_predict_city_code.h5")
+
+# Load optimized model (glglgl)
+gridsearch_opt_model = tf.keras.models.load_model("/home/plkn/repos/machine-learning/chernair/trained_models/model_predict_city_code.h5")
+
+# Get predictions
+predictions = gridsearch_opt_model.predict(X_test)
+
+# Get predicted city code
+predicted_city_code = predictions.argmax(axis=1).astype(np.int)
+
+# Get confusion matrix
+conf_mat = confusion_matrix(y_test, predicted_city_code)
+
+# Get model accuracy
+model_accuracy = accuracy_score(y_test, predicted_city_code, normalize=True)
+
+# Get precision
+model_precision = precision_score(y_test, predicted_city_code, average=None)
+
+
+# Build a new, custom model (sandbox)
+new_model = build_model(
+    n_hidden=4,
+    n_neurons=1000,
+    learning_rate=0.01,
+    input_shape=[4],
+)
+
+# Fit the new model
+new_model.fit(
+    X_train,
+    y_train,
+    epochs=10000,
+    validation_data=(X_train_valid, y_train_valid),
+    callbacks=[keras.callbacks.EarlyStopping(patience=300)],
+)
+
+# Get predictions
+predictions = new_model.predict(X_test)
+
+# Get predicted city code
+predicted_city_code = predictions.argmax(axis=1).astype(np.int)
+
+# Get confusion matrix
+conf_mat = confusion_matrix(y_test, predicted_city_code)
+
+# Get model accuracy
+model_accuracy = accuracy_score(y_test, predicted_city_code, normalize=True)
+
+# Get precision
+model_precision = precision_score(y_test, predicted_city_code, average=None)
+
