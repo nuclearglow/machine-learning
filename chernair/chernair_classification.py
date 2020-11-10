7918c066f61f1d699997516598452153e80f85f0
Sven Vowe <svenvowe@gmail.com>
2020-11-10 23:31:56 +0100

    script lean

--- chernair/chernair_classification.py
+++ chernair/chernair_classification.py
@@ -30,7 +30,7 @@ preprocessed_data_path = os.path.abspath(
 # Read data
 data = joblib.load(preprocessed_data_path)
 
-# X-data: cherntime, isotope-konzentrations 
+# X-data: cherntime, isotope-konzentrations
 X = data[
     ["cherntime", "I-131_interpolated", "Cs-134_interpolated", "Cs-137_interpolated"]
 ].to_numpy()
@@ -43,7 +43,9 @@ X = scaler.fit_transform(X)
 y = data["city_code"].to_numpy()
 
 # Split data in stratified manner
-X_train_full, X_test, y_train_full, y_test = train_test_split(X, y, test_size=0.2, stratify=y)
+X_train_full, X_test, y_train_full, y_test = train_test_split(
+    X, y, test_size=0.2, stratify=y
+)
 X_train, X_train_valid, y_train, y_train_valid = train_test_split(
     X_train_full, y_train_full, test_size=0.3, stratify=y_train_full
 )
@@ -71,79 +73,79 @@ def build_model(n_hidden=1, n_neurons=30, learning_rate=3e-3, input_shape=[4]):
     model.summary()
     return model
 
+
 # use a wrapper to get a scikit learn RegressorModel for the Keras factory method
-keras_clf = keras.wrappers.scikit_learn.KerasClassifier(
-    build_fn=build_model, nb_epoch=100, batch_size=32, verbose=True
-)
+# keras_clf = keras.wrappers.scikit_learn.KerasClassifier(
+#     build_fn=build_model, nb_epoch=100, batch_size=32, verbose=True
+# )
 
 # parameter range for randomized search
-parameter_distribution = {
-    "n_hidden": list(range(6)),
-    "n_neurons": [5, 10, 20, 50, 100, 200, 500, 1000],
-    "learning_rate": [0.1, 0.05, 0.01, 0.005, 0.001, 0.0005, 0.0001],
-    "input_shape": [X_train.shape[1]],
-}
+# parameter_distribution = {
+#     "n_hidden": list(range(6)),
+#     "n_neurons": [5, 10, 20, 50, 100, 200, 500, 1000],
+#     "learning_rate": [0.1, 0.05, 0.01, 0.005, 0.001, 0.0005, 0.0001],
+#     "input_shape": [X_train.shape[1]],
+# }
 
 # Init grid-search
-grid_search_cv = GridSearchCV(keras_clf, parameter_distribution, cv=3)
+# grid_search_cv = GridSearchCV(keras_clf, parameter_distribution, cv=3)
 
 # Perform grid search
-grid_search_cv.fit(
-    X_train,
-    y_train,
-    epochs=100,
-    validation_data=(X_train_valid, y_train_valid),
-    callbacks=[keras.callbacks.EarlyStopping(patience=10)],
-)
+# grid_search_cv.fit(
+#     X_train,
+#     y_train,
+#     epochs=100,
+#     validation_data=(X_train_valid, y_train_valid),
+#     callbacks=[keras.callbacks.EarlyStopping(patience=10)],
+# )
 
-# Get best params
-best_params = grid_search_cv.best_params_
-best_score = grid_search_cv.best_score_
+# # Get best params
+# best_params = grid_search_cv.best_params_
+# best_score = grid_search_cv.best_score_
 
-# Build a fresh optimized model
-optimized_model = build_model(
-    n_hidden=best_params["n_hidden"],
-    n_neurons=best_params["n_neurons"],
-    learning_rate=best_params["learning_rate"],
-)
+# # Build a fresh optimized model
+# optimized_model = build_model(
+#     n_hidden=best_params["n_hidden"],
+#     n_neurons=best_params["n_neurons"],
+#     learning_rate=best_params["learning_rate"],
+# )
 
-# Fit the optimized model
-optimized_model.fit(
-    X_train,
-    y_train,
-    epochs=1000,
-    validation_data=(X_train_valid, y_train_valid),
-    callbacks=[keras.callbacks.EarlyStopping(patience=10)],
-)
+# # Fit the optimized model
+# optimized_model.fit(
+#     X_train,
+#     y_train,
+#     epochs=1000,
+#     validation_data=(X_train_valid, y_train_valid),
+#     callbacks=[keras.callbacks.EarlyStopping(patience=10)],
+# )
 
 # Save the optimized model
-optimized_model.save("trained_models/model_predict_city_code.h5")
+# optimized_model.save("trained_models/model_predict_city_code.h5")
 
 # Load optimized model (glglgl)
-gridsearch_opt_model = tf.keras.models.load_model("/home/plkn/repos/machine-learning/chernair/trained_models/model_predict_city_code.h5")
+# gridsearch_opt_model = tf.keras.models.load_model(
+#     f"{os.getcwd()}/trained_models/model_predict_city_code.h5"
+# )
 
 # Get predictions
-predictions = gridsearch_opt_model.predict(X_test)
+# predictions = gridsearch_opt_model.predict(X_test)
 
 # Get predicted city code
-predicted_city_code = predictions.argmax(axis=1).astype(np.int)
+# predicted_city_code = predictions.argmax(axis=1).astype(np.int)
 
 # Get confusion matrix
-conf_mat = confusion_matrix(y_test, predicted_city_code)
+# conf_mat = confusion_matrix(y_test, predicted_city_code)
 
 # Get model accuracy
-model_accuracy = accuracy_score(y_test, predicted_city_code, normalize=True)
+# model_accuracy = accuracy_score(y_test, predicted_city_code, normalize=True)
 
 # Get precision
-model_precision = precision_score(y_test, predicted_city_code, average=None)
+# model_precision = precision_score(y_test, predicted_city_code, average=None)
 
 
 # Build a new, custom model (sandbox)
 new_model = build_model(
-    n_hidden=4,
-    n_neurons=1000,
-    learning_rate=0.01,
-    input_shape=[4],
+    n_hidden=4, n_neurons=1000, learning_rate=0.01, input_shape=[4],
 )
 
 # Fit the new model
