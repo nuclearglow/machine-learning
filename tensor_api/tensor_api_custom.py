7c296decfc34f7b7146d0223d593438968953b62
Stefan Arnau <fischmechanik@gmail.com>
2020-12-22 22:38:53 +0100

    tesortrouble

--- tensor_api/tensor_api_custom.py
+++ tensor_api/tensor_api_custom.py
@@ -9,6 +9,7 @@ from tensorflow import keras
 from sklearn.model_selection import train_test_split
 
 # Helper libraries
+import sys
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -67,10 +68,60 @@ model = keras.Model(inputs=[input_layer], outputs=[output_layer])
 # Print summmary
 model.summary()
 
+tf.config.experimental_run_functions_eagerly(True)
+
+# custom loss function, proportion of wrong predictions
+v1 = tf.constant([[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]])
+v2 = tf.constant(np.random.rand(10, 10))
+
+tf.print("v1: ", v1, v1.shape)
+
+# tf.print(v1)
+# tf.print(v2)
+
+# a1 = v1.numpy()
+# a2 = v2.numpy()
+
+
+def custom_loss(y_true, y_pred):
+
+    y_pred = tf.cast(
+        tf.expand_dims(tf.math.argmax(y_pred, axis=1, output_type=tf.int32), 1),
+        dtype=tf.float32,
+    )
+    # tf.print("y_true: ", y_true, y_true.shape, y_true.dtype)
+    # tf.print("y_pred: ", y_pred, y_pred.shape, y_pred.dtype)
+
+    y_delta = tf.math.subtract(y_true, y_pred)
+    # tf.print("y_delta: ", y_delta, y_delta.shape, y_delta.dtype)
+
+    n_nonzero = tf.math.count_nonzero(y_delta, dtype=tf.float32)
+    tf.print("n_nonzero: ", n_nonzero, n_nonzero.shape, n_nonzero.dtype)
+
+    # tmp = tf.shape(y_true)[0]
+    # tf.print("tmp: ", tmp, tmp.dtype)
+
+    n_observations = tf.shape(y_true, out_type=tf.float32)
+    tf.print(
+        "n_observations: ", n_observations, n_observations.shape, n_observations.dtype
+    )
+
+    prop_wrong = tf.math.divide(n_nonzero, n_observations)
+    tf.print("prop_wrong: ", prop_wrong, prop_wrong.shape, prop_wrong.dtype)
+
+    # y_true1 = tf.cast(y_true, dtype=tf.int32)
+    # delta = y_true1 - y_pred1
+    # n_nonzero = tf.math.count_nonzero(delta)
+    return prop_wrong
+
+
+# y_pred, y_delta = custom_loss(v1, v2)
+
+# print("Test", loss, output_stream=sys.stdout)
 # compile with options
 model.compile(
     # TODO: custom loss
-    loss="sparse_categorical_crossentropy",
+    loss=custom_loss,
     # TODO: custom metrics
     metrics=["accuracy"],
     optimizer=tf.keras.optimizers.Nadam(
